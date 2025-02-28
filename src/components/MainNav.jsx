1dc0d65c00c04e042e152e36d97adffcc6a07412
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-28 14:26:19 +0100

    Create structure Components

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,5 @@
+export default function MainNav() {
+    return (
+        <h1>MainNav</h1>
+    )
+};
---
+++ src/pages/Products/SingleProduct.jsx
@@ -0,0 +1,5 @@
+export default function SingleProduct() {
+    return (
+        <h1>Pagina Singolo prodotto</h1>
+    )
+};

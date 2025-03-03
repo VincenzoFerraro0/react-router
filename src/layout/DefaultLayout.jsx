6545137b5ab536b4d5ea4098034d01e7305ca091
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-03 16:39:38 +0100

    I define the route of the single post page in app.jsx

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,15 +1,23 @@
-import Footer from "../section/Footer";
+// Importa il componente Header che contiene la navigazione
 import Header from "../section/Header";
-import { Outlet } from "react-router-dom"
 
+// Importa il componente Footer che sarà mostrato in tutte le pagine
+import Footer from "../section/Footer";
 
- 
+// Importa Outlet da React Router per rendere dinamico il contenuto della pagina
+import { Outlet } from "react-router-dom";
+
 export default function DefaultLayout() {
     return (
         <>
+            {/* Header fisso per tutte le pagine */}
             <Header />
+
+            {/* Outlet è il punto in cui verranno renderizzate le pagine dinamiche */}
             <Outlet />
+
+            {/* Footer fisso per tutte le pagine */}
             <Footer />
         </>
-    )
+    );
 };

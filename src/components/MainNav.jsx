66fcd3ee65a27072cd907fd8bf1ac8bd4971a175
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-03 16:23:56 +0100

    install axios and create file .env

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -21,9 +21,6 @@ export default function MainNav() {
             
             {/* <!-- Icona menu mobile --> */}
             <button className="md:hidden text-gray-700 focus:outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
-                </svg>
             </button>
         </div>
     </nav>

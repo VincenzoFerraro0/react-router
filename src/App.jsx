97908c3bd8d42dd5e6c255ae7a1ed2205247cef9
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-03 16:40:03 +0100

    I define the route of the single post page in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
+// Importa le componenti necessari da React Router per gestire la navigazione
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// PAGES
+// Pagine da importare
 import HomePage from './pages/HomePage';
 import AboutUs from './pages/AboutUs';
-import Posts from './pages/Posts/Posts'
+import Posts from './pages/Posts/Posts';
+import SinglePost from './pages/Posts/SinglePost';
 
-// LAYOUTS
+// Layout predefinito da usare in tutta l'app
 import DefaultLayout from './layout/DefaultLayout';
 
 function App() {
-
   return (
     <>
+      {/* Il BrowserRouter è il componente che gestisce la navigazione del router */}
       <BrowserRouter>
         <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/posts" element={<Posts />} />
+          {/* Definisce una route che utilizza il layout predefinito */}
+          <Route Component={ DefaultLayout }>
+            {/* Definisce le rotte specifiche per le pagine */}
+            <Route path="/" Component={ HomePage } />               {/* Home Page */}
+            <Route path="/about-us" Component={ AboutUs } />         {/* Pagina Chi Siamo */}
+            <Route path="/posts" Component={ Posts } />              {/* Lista dei Post */}
+            <Route path="/posts/:id" Component={ SinglePost } />     {/* Pagina Singolo Post */}
           </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

5271f771507ed926d85c338e8e59a493a86e7b38
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-03 16:34:36 +0100

    add component PostCard into page Posts.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,15 @@
 export default function PostCard({ id, title, content, tags, url }) {
     return (
         <div key={id}>
+             {/* Titolo del post */}
             <h3 >{title}</h3>
+            {/* Immagine del post */}
             <figure >
                 <img src={url} alt={title}/>
             </figure>
+            {/* Contenuto del post */}
             <p>{content}</p>
+            {/* Lista dei tag (se presenti) */}
             {tags && Array.isArray(tags) && (
                 <ul>
                     {tags.map((tag, index) => (
--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,5 +1,46 @@
-export default function posts() {
+// Importa gli hook di React per gestire lo stato e gli effetti collaterali
+import { useEffect, useState } from "react";
+
+// Importa Axios per effettuare richieste HTTP
+import axios from "axios";
+
+// Importa NavLink per creare link di navigazione dinamici
+import { NavLink } from 'react-router-dom';
+
+// Importa il componente PostCard per visualizzare ogni post
+import PostCard from "../../components/PostCard";
+
+export default function Posts() {
+    // Stato per memorizzare la lista dei post
+    const [posts, setPosts] = useState([]);
+
+    // URL dell'API definito nelle variabili d'ambiente
+    const url = import.meta.env.VITE_ENDPOINT_URL;
+
+    // Effettua una richiesta HTTP all'API quando il componente viene montato
+    useEffect(() => {
+        axios.get(url)
+            .then((res) => setPosts(res.data)) // Salva i dati ricevuti nello stato `posts`
+    }, []); // Il secondo parametro `[]` assicura che la chiamata venga eseguita solo una volta
+
     return (
-        <h1>Pagina Dei posts</h1>
-    )
+        <div>
+            {/* Mappa i post ricevuti e crea una card per ognuno */}
+            <div>
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        {/* Link al singolo post usando il suo ID */}
+                        <NavLink to={`${post.id}`}>
+                            <PostCard 
+                                title={post.title} 
+                                content={post.content} 
+                                tags={post.tags} 
+                                url={post.url} 
+                            />
+                        </NavLink>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };

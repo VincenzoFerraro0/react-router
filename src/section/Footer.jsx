4660fb518bc824de543c86c6ea816308790585c7
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-03 16:54:32 +0100

    Create "Footer"

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -1,7 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 export default function Footer() {
     return (
         <footer>
-            <h1>footer</h1>
+            <div className="bg-black shadow-md p-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    {/* <!-- Logo --> */}
+                    <a href="#" className="text-2xl font-bold text-blue-600">Blog</a>
+
+                    {/* <!-- Links di navigazione --> */}
+                    <ul className="hidden md:flex space-x-6">
+                        <li><NavLink to="/" className="text-gray-700 hover:text-blue-600">Home</NavLink></li>
+                        <li><NavLink to="/about-us" className="text-gray-700 hover:text-blue-600">Chi Siamo</NavLink></li>
+                        <li><NavLink to="/posts" className="text-gray-700 hover:text-blue-600">Posts</NavLink></li>
+                    </ul>
+                    {/* <!-- Bottone Login --> */}
+                    <div className="hidden md:block">
+                        <a href="#" className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">Login</a>
+                    </div>
+                </div>
+
+            </div>
         </footer>
     )
 };
